851f9b6dcf9bb854a48e4ed5cd5a4bfe4a25ff11
Gregory Holder <gregory.holder@equans.com>
2026-06-09 10:13:58 +0200

    zsh: add vi-mode cursor and keybinding tweaks

--- config-programs/shell/keybindings.nix
+++ config-programs/shell/keybindings.nix
@@ -1,6 +1,34 @@
 { ... }:
 {
   programs.zsh.initExtra = ''
+    # Use vi mode in zsh and switch cursor shape with the active keymap.
+    KEYTIMEOUT=1
+    bindkey -v
+
+    function zle-keymap-select {
+      if [[ $KEYMAP == vicmd ]]; then
+        print -n -- $'\e[2 q' # steady block cursor (normal mode)
+      else
+        print -n -- $'\e[6 q' # steady beam cursor (insert mode)
+      fi
+    }
+    zle -N zle-keymap-select
+
+    function zle-line-init {
+      zle-keymap-select
+    }
+    zle -N zle-line-init
+
+    function zle-line-finish {
+      print -n -- $'\e[6 q'
+    }
+    zle -N zle-line-finish
+
+    # Keep Ctrl+F accepting autosuggestions in vi insert mode.
+    bindkey -M viins '^F' forward-char
+    bindkey -M emacs '^F' forward-char
+    bindkey -M viins '^U' backward-kill-line
+
     # Keybindings for common terminal actions
     bindkey  "^[[H"   beginning-of-line
     bindkey  "^[[F"   end-of-line
