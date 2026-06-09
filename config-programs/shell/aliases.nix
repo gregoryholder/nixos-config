4498d3a6556ead260e69d086b281041d5f022cbc
Gregory Holder <gregory.holder@equans.com>
2026-06-09 10:12:25 +0200

    comp aliases

--- config-programs/shell/aliases.nix
+++ config-programs/shell/aliases.nix
@@ -26,6 +26,9 @@
     sudo = "sudo -E";
     rga = "rga -g '!~$*' -g '!*.zip' -g '!*.tar'";
     comp_lint = ''bcompare WarningsCpplint_merge-base.txt WarningsCpplint.txt -title1="Cpplint [Merge base]" -title2="Cpplint [Current]"'';
+    comp_check = ''bcompare WarningsCppcheck_merge-base.xml WarningsCppcheck.xml -title1="Cppcheck [Merge base]" -title2="Cppcheck [Current]"'';
+    comp_dox = ''bcompare WarningsDoxygen-vehicule_merge-base.txt WarningsDoxygen-vehicule.txt -title1="Doxygen [Merge base]" -title2="Doxygen [Current]"'';
+    comp_lizard = ''bcompare WarningsLizard_merge-base.txt WarningsLizard.txt -title1="Lizard [Merge base]" -title2="Lizard [Current]"'';
   };
 
   programs.zsh.zsh-abbr = {
