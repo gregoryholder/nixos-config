79d4ad0b8e0225a05c35abd5e06932e0998a3b4b
Gregory Holder <gregory.holder@equans.com>
2026-06-10 10:25:05 +0200

    add zsh startup profiling and cached compinit

--- config-programs/shell/zsh.nix
+++ config-programs/shell/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./keybindings.nix
@@ -10,11 +10,22 @@
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
+    initExtraFirst = ''
+      if [[ -n "$ZSH_PROFILE_STARTUP" ]]; then
+        zmodload zsh/zprof
+      fi
+    '';
+    initExtra = lib.mkAfter ''
+      if [[ -n "$ZSH_PROFILE_STARTUP" ]]; then
+        zprof
+      fi
+    '';
     enableCompletion = true;
     completionInit = ''
       FPATH="$HOME/.docker/completions:$FPATH"
 
-      autoload -Uz +X compinit && compinit
+      mkdir -p "''${XDG_CACHE_HOME:-$HOME/.cache}/zsh"
+      autoload -Uz +X compinit && compinit -d "''${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump" -C
       autoload -Uz +X bashcompinit && bashcompinit
       source /etc/bash_completion.d/*
     '';
