6e7f726eefaabac94b69fef814582eb9b0d4a50d
Gregory Holder <gregory.holder@equans.com>
2026-09-07 09:21:33 +0200

    feat: add big_monitor display configuration alias

--- work/default.nix
+++ work/default.nix
@@ -26,5 +26,7 @@
     cc_veh = "ln -fs TargetLinuxHost/VehiculeUnityBuildsDebug/compile_commands.json .";
     bc_check = "bcompare WarningsCppcheck_merge-base.xml WarningsCppcheck.xml";
     bc_dox = "bcompare WarningsDoxygen-vehicule_merge-base.txt WarningsDoxygen-vehicule.txt";
+
+    big_monitor = ''xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync && xrandr --addmode DP-1 "2560x1440_60.00" && xrandr --output DP-1 --mode 2560x1440_60.00'';
   };
 }
